66253904fff24e75d3cd579125f06911fcb587c1
yuxuan <zyx97agm@gmail.com>
2021-05-16 11:44:43 +0000

    add passport library

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").strategy;
 
 const app = express();
+
+// 告诉passport用specific service去处理authentication
+passport.use(new GoogleStrategy())
 
 app.get('/', (req, res) => {
 	res.send({ words: 'hi there' });
