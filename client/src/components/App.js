31424672eb6d865e1fbd4361d5cd03e7849bd937
Kevin Zhu <zyx97agm@gmail.com>
2021-06-25 12:08:28 +0000

    initialize react redux and browserrouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const pageOne = () => {
+	return <div>PageOne</div>;
+};
 
 
-function App() {
+
+const App = () => {
 	// 	在prod阶段href = "/auth/google"
 	// 	reactRedirectURL只是用于dev阶段
 	const reactRedirectURL = 'https://emailyserver.run-us-west2.goorm.io/auth/google/';
 	return (
 		<div>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route path="/" component={pageOne} exact></Route>
+				</div>
+			</BrowserRouter>
 			<h1>
 				<a href={reactRedirectURL}>Click me</a>
 			</h1>
 		</div>
 	);
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux"
+import {createStore, applyMiddleware, compose} from "redux"
+import reducers from "./reducers/"
 
-import App from './App';
+import App from './components/App';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 );
