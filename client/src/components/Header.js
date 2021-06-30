e6a475240f3e99ba04c04b26a4fe5851b0575c5a
Kevin Zhu <zyx97agm@gmail.com>
2021-06-30 16:58:50 +0000

    finish backend charge part

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -42,7 +42,7 @@ class Header extends React.Component {
 	}
 
 	render() {
-		console.log(this.props);
+		// console.log(this.props);
 		return (
 			<>
 				<nav>
--- routes/billingRoute.js
+++ routes/billingRoute.js
@@ -3,6 +3,17 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = (app) => {
 	app.post('/api/stripe', (req, res) => {
-		res.send({"name": "yuxuan"})
+		// 	create the actual charge, bill the credit, response back
+		stripe.charges.create({
+			amount: 500,
+			currency: 'usd',
+			description: '$5 for 5 credits',
+			source: req.body.id,
+		}, function(err, charge){
+			if(err){
+				console.log(err);
+			}
+			console.log("******charge successfully******")
+		});
 	});
 };
