97b4dbb9f32beedb4b06fdb38c4dfff92537d1eb
Kevin Zhu <zyx97agm@gmail.com>
2021-06-30 12:19:45 +0000

    finish handleToken action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,7 +7,7 @@ export const fetchUser = () => {
 		const response = await axios.get(
 			'https://emailyserver.run-us-west2.goorm.io/api/current_user',
 			{
-// 				axios默认是发送请求的时候不会带上cookie的，需要通过设置withCredentials: true来解决
+				// 	axios默认是发送请求的时候不会带上cookie的，需要通过设置withCredentials: true来解决
 				withCredentials: true,
 			}
 		);
@@ -15,5 +15,22 @@ export const fetchUser = () => {
 			type: FETCH_USER,
 			payload: response.data,
 		});
+	};
+};
+
+// send the token to back end server and then get updated 
+// user model back. finally dispatch the user model to the reducer
+export const handleToken = (token) => {
+	return async (dispatch) => {
+		const response = await axios.post(
+			'https://emailyserver.run-us-west2.goorm.io/api/stripe',
+			token,
+			{ withCredentials: true }
+		);
+		
+		dispatch({
+			type: FETCH_USER,
+			payload: response.data
+		})
 	};
 };
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { handleToken } from '../actions';
+import { connect } from 'react-redux';
 
 class Payments extends React.Component {
 	render() {
 		return (
 			<StripeCheckout
+				name="EmailApp"
+				description="$5 for 5 credits"
 				amount={500}
-				token={(token) => console.log(token)}
+				token={(token) => this.props.handleToken(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_KEY}
-			/>
+			></StripeCheckout>
 		);
 	}
 }
 
-export default Payments;
+export default connect(null, { handleToken })(Payments);
