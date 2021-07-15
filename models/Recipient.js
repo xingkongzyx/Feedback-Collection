1841e583b25a01da8b024cc1495279153fc8c4f3
Kevin Zhu <zyx97agm@gmail.com>
2021-07-15 14:48:50 +0000

    update survey data in mongo

--- models/Recipient.js
+++ models/Recipient.js
@@ -1,8 +1,10 @@
+// set up sub docuemnt for recipients in surveySchema
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const recipientSchema = new Schema({
 	email: String,
+// 	has the user responded to the survey(true/false)
 	responded: { type: Boolean, default: false },
 });
 
--- models/Survey.js
+++ models/Survey.js
@@ -18,3 +18,5 @@ const surveySchema = new Schema({
 });
 
 mongoose.model('surveys', surveySchema);
+
+// 3.19
